e1969cfcf4efcf3d86dcebd526cf35675bad350c
Håkan Öström <hakan.ostrom@gmail.com>
2024-04-18 15:54:53 +0200

    cleanup

--- app/src/main/java/se/hakanostrom/filmappen/database/FilmappenDatabas.java
+++ app/src/main/java/se/hakanostrom/filmappen/database/FilmappenDatabas.java
@@ -3,12 +3,9 @@ package se.hakanostrom.filmappen.database;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-import se.hakanostrom.filmappen.entity.User;
 import se.hakanostrom.filmappen.model.Favoritfilm;
 
-@Database(entities = {User.class, Favoritfilm.class}, version = 1)
+@Database(entities = {Favoritfilm.class}, version = 1)
 public abstract class FilmappenDatabas extends RoomDatabase {
     public abstract FavoritfilmDao favoritfilmDao();
-
-    public abstract UserDao userDao();
 }
