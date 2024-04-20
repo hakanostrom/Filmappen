24060b1b9c39805967290fbbf15a18dd428e2ace
Håkan Öström <hakan.ostrom@gmail.com>
2024-04-20 19:45:50 +0200

    Simple unit test

---
+++ app/src/test/java/se/hakanostrom/filmappen/model/FavoritfilmTest.java
@@ -0,0 +1,40 @@
+package se.hakanostrom.filmappen.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FavoritfilmTest {
+
+    private final String FAVORITFILM_TITLE = "Min favoritfilm";
+    private final String FAVORITFILM_TYPE = "Movie";
+    private final String FAVORITFILM_YEAR = "2014";
+    private final int FAVORITFILM_VALTBETYG = 5;
+
+    Favoritfilm favoritfilm;
+
+    @Before
+    public void setUp() throws Exception {
+        favoritfilm = new Favoritfilm()
+                .setTitle(FAVORITFILM_TITLE)
+                .setType(FAVORITFILM_TYPE)
+                .setYear(FAVORITFILM_YEAR)
+                .setValtBetyg(FAVORITFILM_VALTBETYG);
+    }
+
+    @Test
+    public void testGetterSetter() {
+        assertEquals(FAVORITFILM_TITLE, favoritfilm.getTitle());
+        assertEquals(FAVORITFILM_TYPE, favoritfilm.getType());
+        assertEquals(FAVORITFILM_YEAR, favoritfilm.getYear());
+        assertEquals(FAVORITFILM_VALTBETYG, favoritfilm.getValtBetyg());
+    }
+
+    @Test
+    public void testToString() {
+        String expected = String.format("%s [%s] (%s)", FAVORITFILM_TITLE, FAVORITFILM_TYPE, FAVORITFILM_YEAR);
+
+        assertEquals(expected, favoritfilm.toString());
+    }
+}
---
+++ app/src/test/java/se/hakanostrom/filmappen/model/SearchResultTest.java
@@ -0,0 +1,28 @@
+package se.hakanostrom.filmappen.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SearchResultTest {
+
+    private final String SINGLERESULT_TITLE = "Min favoritfilm";
+    private final String SINGLERESULT_YEAR = "2014";
+
+    SearchResult.SingleResult singleResult;
+
+    @Before
+    public void setUp() throws Exception {
+        singleResult = new SearchResult.SingleResult();
+        singleResult.title = SINGLERESULT_TITLE;
+        singleResult.year = SINGLERESULT_YEAR;
+    }
+
+    @Test
+    public void testToString() {
+        String expected = String.format("%s (%s)", SINGLERESULT_TITLE, SINGLERESULT_YEAR);
+
+        assertEquals(expected, singleResult.toString());
+    }
+}
